aa447a34fc85cabde53514798357402c8f0d2fad
sakshianand <sakku.anand@gmail.com>
2016-12-29 14:10:47 +0530

    changes

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 var React = require('react');
 var ReactDOM =require('react-dom');
 var App = require('./components/App');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 ReactDOM.render(<App />,document.getElementById('todoapp'));
